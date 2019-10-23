0836c7d450f5dfcc23233a924b5751d0879cd598
Martyn den Toom <mdentoom@mac.com>
2019-10-23 20:57:52 +0200

    Opdrachten W3D3-3 en Opdrachten wk 4 tot W4D3

--- Week3/W3D3/W3D3-3/script.js
+++ Week3/W3D3/W3D3-3/script.js
@@ -13,3 +13,20 @@ const setMovieGenres = function(movieData) {
     document.getElementById('movieGenresList').append(listItem);
   });
 };
+
+//script.js
+// getMovieGenres().then(responseJSON => {
+//   const movies = responseJSON;
+//   setMovieGenres(movies);
+//   console.log('hier is je data om iets mee te gaan doen:', responseJSON);
+// });
+
+// const setMovieGenres = function(movieData) {
+//   const moviesGenres = movieData.genres;
+//   moviesGenres.forEach(movieGenre => {
+//     const listItem = document.createElement('li');
+//     const listItemContent = `genre naam: ${movieGenre.name}, id: ${movieGenre.id}`;
+//     listItem.appendChild(document.createTextNode(listItemContent));
+//     document.getElementById('movieGenresList').append(listItem);
+//   });
+// };
